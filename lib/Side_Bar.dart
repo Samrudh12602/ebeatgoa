490a95d2c8cc922241bb160baaccf98ceb17a40c
Samrudh12602 <samdhaimodkar@gmail.com>
2023-04-19 22:36:35 +0530

    hi

---
+++ lib/Side_Bar.dart
@@ -0,0 +1,8 @@
+class SideBarViewDemo extends StatelessWidget {
+  const SideBarViewDemo({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
