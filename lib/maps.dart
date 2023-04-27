7aab02e75b957a4cafc60d447eb5fd5b7d1e94c6
Jovi12 <jovidsilva6@gmail.com>
2023-04-27 18:00:54 +0530

    jovi

--- lib/maps.dart
+++ lib/maps.dart
@@ -13,23 +13,34 @@ class Maps extends StatefulWidget {
 class _MapsState extends State<Maps> {
   GoogleMapController? _controller;
   Location _location = Location();
-  late BitmapDescriptor _markerIcon;
+  // late BitmapDescriptor _markerIcon;
+  late BitmapDescriptor _markerIcon = BitmapDescriptor.defaultMarker;
+
+
+  @override
+  // void initState() {
+  //   super.initState();
+  //   _location.onLocationChanged.listen((LocationData currentLocation) {
+  //     _controller?.animateCamera(CameraUpdate.newCameraPosition(
+  //       CameraPosition(
+  //         target: LatLng(currentLocation.latitude!, currentLocation.longitude!),
+  //         zoom: 16.0,
+  //       ),
+  //     ));
+  //   });
+  // late BitmapDescriptor _markerIcon;
 
   @override
   void initState() {
     super.initState();
-    _location.onLocationChanged.listen((LocationData currentLocation) {
-      _controller?.animateCamera(CameraUpdate.newCameraPosition(
-        CameraPosition(
-          target: LatLng(currentLocation.latitude!, currentLocation.longitude!),
-          zoom: 16.0,
-        ),
-      ));
-    });
-
     BitmapDescriptor.fromAssetImage(ImageConfiguration(size: Size(48, 48)), 'assets/images/marker.png')
-        .then((icon) => _markerIcon = icon);
+        .then((icon) => setState(() => _markerIcon = icon));
   }
+
+
+  // BitmapDescriptor.fromAssetImage(ImageConfiguration(size: Size(48, 48)), 'assets/images/marker.png')
+  //       .then((icon) => _markerIcon = icon);
+  // }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
