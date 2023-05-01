028f8909646cd70852acd300a33ef7118a35f16c
DNC008 <delidacaldeira@gmail.com>
2023-05-01 23:19:47 +0530

    Delida : Added home with  static count cards of beat officers...

--- lib/home.dart
+++ lib/home.dart
@@ -20,10 +20,12 @@ class _HomePageState extends State<HomePage> {
   String _postOfWork = '';
   final List<Widget> _children = [
     Home(
-      numBeatOfficers: 10,
-      numBeatLocations: 5,
-      numPoliceInspectors: 3,
-      numSuperintendents: 1,
+      numBeatOfficers: 500,
+      numBeatLocations: 400,
+      numPoliceInspectors: 50,
+      numSuperintendents: 3,
+        numBeatAreas: 40,
+        numSubDivisionalOfficers: 10,
     ),
     MapScreen(),
     UserMap(),
@@ -142,94 +144,99 @@ class Home extends StatelessWidget {
   final int numBeatLocations;
   final int numPoliceInspectors;
   final int numSuperintendents;
+  final int numBeatAreas;
+  final int numSubDivisionalOfficers;
 
   Home({
     required this.numBeatOfficers,
     required this.numBeatLocations,
     required this.numPoliceInspectors,
     required this.numSuperintendents,
+    required this.numBeatAreas,
+    required this.numSubDivisionalOfficers,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 40.0),
-            child: Text(
-              'Welcome to the Goa Police App Home Page',
-              style: TextStyle(
-                color: Colors.green,
-                fontSize: 24.0,
-                fontWeight: FontWeight.bold,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Goa Police App Home'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Expanded(
+              child: GridView.count(
+                crossAxisCount: 2,
+                mainAxisSpacing: 10.0,
+                crossAxisSpacing: 10.0,
+                children: [
+                  _buildCard(
+                    'Beat Officers',
+                    numBeatOfficers.toString(),
+                    Colors.blue,
+                  ),
+                  _buildCard(
+                    'Beat Locations',
+                    numBeatLocations.toString(),
+                    Colors.red,
+                  ),
+                  _buildCard(
+                    'Police Inspectors',
+                    numPoliceInspectors.toString(),
+                    Colors.green,
+                  ),
+                  _buildCard(
+                    'Superintendents',
+                    numSuperintendents.toString(),
+                    Colors.orange,
+                  ),
+                  _buildCard(
+                    'Beat Areas',
+                    numBeatAreas.toString(),
+                    Colors.purple,
+                  ),
+                  _buildCard(
+                    'Sub Divisional Officers',
+                    numSubDivisionalOfficers.toString(),
+                    Colors.teal,
+                  ),
+                ],
               ),
             ),
-          ),
-          Expanded(
-            child: GridView.count(
-              crossAxisCount: 2,
-              childAspectRatio: 1.0,
-              mainAxisSpacing: 10.0,
-              crossAxisSpacing: 10.0,
-              padding: const EdgeInsets.symmetric(horizontal: 20.0),
-              children: [
-                _buildCard(
-                  'Beat Officers',
-                  numBeatOfficers.toString(),
-                  Colors.blue,
-                  Icons.people,
-                ),
-                _buildCard(
-                  'Beat Locations',
-                  numBeatLocations.toString(),
-                  Colors.orange,
-                  Icons.location_on,
-                ),
-                _buildCard(
-                  'Police Inspectors',
-                  numPoliceInspectors.toString(),
-                  Colors.pink,
-                  Icons.security,
-                ),
-                _buildCard(
-                  'Superintendents',
-                  numSuperintendents.toString(),
-                  Colors.green,
-                  Icons.assignment_ind,
-                ),
-              ],
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
 
-  Widget _buildCard(String title, String value, Color color, IconData icon) {
+  Widget _buildCard(String title, String subtitle, Color color) {
     return Card(
       elevation: 4.0,
       color: color,
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.0)),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(16.0),
+      ),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Icon(
-            icon,
-            color: Colors.white,
-            size: 40.0,
-          ),
-          SizedBox(height: 20.0),
           Text(
             title,
-            style: TextStyle(color: Colors.white, fontSize: 18.0),
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 24.0,
+              fontWeight: FontWeight.bold,
+            ),
           ),
           SizedBox(height: 10.0),
           Text(
-            value,
-            style: TextStyle(color: Colors.white, fontSize: 24.0),
+            subtitle,
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 20.0,
+            ),
           ),
         ],
       ),
