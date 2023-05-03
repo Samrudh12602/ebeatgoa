f2b618f9fd0fe346f60a80947f30010a281d64a3
Samrudh12602 <samdhaimodkar@gmail.com>
2023-05-04 01:48:07 +0530

    just an addition

--- lib/home.dart
+++ lib/home.dart
@@ -220,9 +220,6 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Goa Police App Home'),
-      ),
       body: Padding(
         padding: EdgeInsets.all(16.0),
         child: Column(
